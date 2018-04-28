ebbe666c762a02eb775e7a1f6e989a4823303cc3
AMasquelier <38529011+AMasquelier@users.noreply.github.com>
2018-04-28 18:05:34 +0200

    Add files via upload

---
+++ libfractal/fractal.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include "fractal.h"
+
+struct fractal *fractal_new(const char *name, int width, int height, double a, double b)
+{
+  	struct fractal *f = malloc(sizeof(struct fractal));
+	f->name = name;
+	f->w = width;
+	f->h = height;
+	f->a = a;
+	f->b = b;
+	f->value
+    return f;
+}
+
+void fractal_free(struct fractal *f)
+{
+    if(f != NULL) free(f);
+}
+
+const char *fractal_get_name(const struct fractal *f)
+{
+    return f->name;
+}
+
+int fractal_get_value(const struct fractal *f, int x, int y)
+{
+    return f->value[x + y * f->w];
+}
+
+void fractal_set_value(struct fractal *f, int x, int y, int val)
+{
+    f->value[x + y * f->w] = val;
+}
+
+int fractal_get_width(const struct fractal *f)
+{
+    return f->w;
+}
+
+int fractal_get_height(const struct fractal *f)
+{
+    return f->h;
+}
+
+double fractal_get_a(const struct fractal *f)
+{
+    return f->a;
+}
+
+double fractal_get_b(const struct fractal *f)
+{
+    return f->b;
+}
